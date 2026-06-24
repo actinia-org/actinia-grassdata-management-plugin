2ec64acb59d9a605674c0605e0b47810ac96e042
TaniaG <tania_93xime@hotmail.com>
2026-06-24 13:14:14 +0200

    fix assertion error

--- tests/test_raster_upload.py
+++ tests/test_raster_upload.py
@@ -57,7 +57,7 @@ class UploadRasterLayerTestCase(ActiniaResourceTestCaseBase):
     local_raster = f"/tmp/{raster}.tif"
 
     ref_info = {
-        "cells": "225000",
+        "cells": "226904",
         "cols": "500",
         "east": "645000",
         "ewres": "30",
